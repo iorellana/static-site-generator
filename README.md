aa3460ce1324e965881b60e95c466ad7fe346e0c
iorellana <nacho@foxdev.com.ar>
2024-08-11 05:31:01 +0000

    Readme and new feature added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# static-site-generator
+# Static Site Generator
+
+## Description
+This project is a static site generator that converts Markdown files into HTML. It is a command-line tool that takes a directory of Markdown files and converts them into HTML files. The tool also supports custom templates, allowing users to customize the look and feel of their generated website.
+
+## Features
+- Converts Markdown files to HTML
+- Supports custom templates
+- Generates static websites quickly and efficiently
+
+## Installation
+Not much installation needed. Just clone the repository and run the project.
+
+# Example command to run your project
+python main.py
--- src/converter.py
+++ src/converter.py
@@ -21,6 +21,20 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(parts[i], text_type))
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        text = node.text
+        parts = extract_markdown_images(text)
+        if len(parts) == 0:
+            new_nodes.append(node)
+            continue
+        for i in range(0, len(parts)):
+            if i == 0:
+                new_nodes.append(TextNode(parts[i][0], node.text_type))
+            new_nodes.append(TextNode(parts[i][1], TextType.text_type_image))
+    return new_nodes
+
 def extract_markdown_images(text):
     matches = re.findall(r"!\[([^\]]*)\]\(([^)]*)\)", text)
     return matches
