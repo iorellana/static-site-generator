5df0ce631dd6b711397653d761fd37e0005f52be
iorellana <nacho@foxdev.com.ar>
2024-08-14 04:08:29 +0000

    build and copy files to public folder

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
-print("hi")
+from os import listdir, mkdir
+from os.path import exists, join, isfile
+from shutil import copy, rmtree
+
+def main():
+    # copy everything from static to public
+    static_dir = "static"
+    public_dir = "public"
+    if exists(public_dir):
+        rmtree(public_dir)
+    mkdir(public_dir)
+    copy_recursive(static_dir, public_dir)
+
+def copy_recursive(src, dst):
+    if isfile(src):
+        copy(src, dst)
+    else:
+        if not exists(dst):
+            mkdir(dst)
+        for l in listdir(src):
+            copy_recursive(join(src, l), join(dst, l))
+
+main()
